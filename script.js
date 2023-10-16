10f51716abc215e07615172fef0cf3a51b127c01
ZiadRabea <138338114+ZiadRabea@users.noreply.github.com>
2023-10-16 12:02:40 +0300

    new_update

--- script.js
+++ script.js
@@ -27,63 +27,6 @@ let jsArrow = document.querySelector(".js-arrow");
 jsArrow.onclick = function() {
  navLinks.classList.toggle("show3");
 }
-// select video element
-var vid = document.getElementById('scroll-video');
-var isScrolling = false;
-
-// Function to pause the video
-function pauseVideo() {
-    if (!isScrolling) {
-        vid.pause();
-        isScrolling = true;
-    }
-}
-
-// Debounce the scroll event to avoid excessive calls
-var scrollTimeout;
-if (window.innerWidth > 700){
-window.addEventListener('scroll', function () {
-    clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(function () {
-        pauseVideo();
-        isScrolling = false;
-    }, 100); // Adjust the debounce delay as needed
-})};
-var active_animation = true
-// Function to update video frame on requestAnimationFrame
-function updateVideoFrame() {
-    vid.currentTime = (window.scrollY / window.innerHeight) * 13;
-    if (!isScrolling) {
-        requestAnimationFrame(updateVideoFrame);
-    }
-    if (active_animation) {
-        if (window.scrollY >= 1000){
-            vid.style.display = "none"
-            document.getElementById("container").style.margin = 0
-            window.scrollTo({
-                top:0,
-                behavior:"smooth"
-            })
-            active_animation = false
-        }
-    }
-}
-
-// Initial call to start updating video frame
-if (window.innerWidth > 700){
-    updateVideoFrame();
-}
-else{
-    vid.play()
-    document.getElementById("container").style.margin = 0
-    setTimeout(() => {
-        vid.style.display = "none"
-        window.scrollTo({
-            top:0,
-            behavior:"smooth"
-        })
-    }, 13000);
-}
 // JavaScript for handling navigation arrows and bullets
 let images = ["assets/code1.png", "assets/code2.png", "assets/code3.png", "assets/code4.png", "assets/code5.png"]
 let des = ["Hello world application in world language (English)", "Application Hello World dans la langue du monde (français)", "Dünya dilinde Merhaba Dünya uygulaması (Türkçe)", "世界言語 (トルコ語) の Hello world アプリケーション", "تطبيق أهلاً بالعالم بلغة العالم (العربية)"]
